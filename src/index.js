60be65893ba9cff135b7cf9387e5a5ef47a47444
Daniel Corcoran <danielc.92@outlook.com>
2019-08-26 18:09:36 +1000

    Added redux store

--- src/index.js
+++ src/index.js
@@ -1,6 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
+import { createStore, compose, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
 
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const temporaryReducer = () => {
+    return {state: "hello world"}
+}
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(temporaryReducer, /* preloaded state, */ composeEnhancers(
+    applyMiddleware(thunk)
+))
+
+ReactDOM.render(
+    <App store={store}/>, 
+    document.getElementById('root'));
