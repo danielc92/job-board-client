a40edf834934ca1450cf54ffbdab7321962f1e18
Daniel Corcoran <danielc.92@outlook.com>
2019-08-26 18:53:41 +1000

    Added placeholder for legal

---
+++ src/components/TermsAndConditions.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Header from 'semantic-ui-react';
+
+function TermsAndConditions() {
+    return (
+        <div>
+            <Header as="h2"></Header>
+            <p>Sunt voluptate sint irure laborum ut ullamco qui. Ex est anim eu aliquip consectetur veniam ut Lorem mollit. Quis aute Lorem exercitation culpa Lorem aliqua minim. Officia ad aliqua enim nostrud cupidatat est sit. Sit sunt dolor do veniam adipisicing culpa excepteur sint aute. Ut proident duis tempor elit incididunt. Eu dolor dolor voluptate commodo excepteur pariatur fugiat cupidatat magna in anim commodo.
+
+Mollit minim ex culpa quis sint in culpa do occaecat cupidatat veniam commodo consectetur laborum. Ad excepteur nostrud voluptate aliqua aliqua. Magna occaecat cupidatat sunt eu dolore reprehenderit ea deserunt culpa enim enim aliqua eu velit. Sint laboris mollit amet consectetur consectetur sunt sint Lorem nisi voluptate Lorem reprehenderit amet. Eu mollit anim proident ad sit incididunt ut ea Lorem ut et laborum mollit nostrud. Nostrud reprehenderit sit duis quis nulla et.
+
+Commodo ullamco culpa aliquip incididunt. Irure cillum labore commodo in ad reprehenderit exercitation labore adipisicing ipsum id officia tempor. Cupidatat aliqua ipsum anim commodo amet cillum proident pariatur. Labore magna nisi laboris id deserunt Lorem adipisicing ut nostrud elit et consectetur velit labore.</p>
+        </div>
+    )
+}
+
+export default TermsAndConditions
