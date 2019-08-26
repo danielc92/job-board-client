ee78fe283f5eee29b854c072ccfcd74b26a4871e
Daniel Corcoran <danielc.92@outlook.com>
2019-08-26 18:59:11 +1000

    Added placeholder nav

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,10 @@
 import React, { Component } from 'react'
+import ReactNav from './ReactNav';
 
 export default class App extends Component {
   render() {
     return (
-      <div>
-        App
-      </div>
+      <ReactNav></ReactNav>
     )
   }
 }
