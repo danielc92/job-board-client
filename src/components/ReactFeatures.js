54ebb2898d101def40e05432babe4828b0869d05
Daniel Corcoran <danielc.92@outlook.com>
2019-08-27 16:15:46 +1000

    Removed broken transition from features

--- src/components/ReactFeatures.js
+++ src/components/ReactFeatures.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Segment, Container, Header, Transition, Grid, Icon} from 'semantic-ui-react';
+import { Segment, Container, Header, Grid, Icon} from 'semantic-ui-react';
 import { connect } from 'react-redux';
 
 class ReactFeatures extends Component {
@@ -36,14 +36,13 @@ class ReactFeatures extends Component {
             <Segment style={{ padding: '7em 0em', margin: '0' }}>
             <Container>
               <Header as="h1" textAlign="center" style={{marginBottom: '5rem'}}>Features</Header>
-              <Transition visible={true} duration={600} animation="scale">
               <Grid columns={4} stackable>
                 <Grid.Row>
                   {
                     featureData.map(item => (
                       
                       <Grid.Column style={{ textAlign: "center"}}>
-                        <Icon name={item.icon} size="big"/>
+                        <Icon color={this.props.theme} name={item.icon} size="big"/>
                         <Header as={featureHeader}>{ item.title }</Header>
                         <p>{ item.content }</p>
                       </Grid.Column>
@@ -52,7 +51,6 @@ class ReactFeatures extends Component {
                   }
                 </Grid.Row>
               </Grid>
-              </Transition>
             </Container>
           </Segment>
         )
