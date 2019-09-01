b0f7f87b135d623b10c2e25e8e05cc260f1cd3c1
Daniel Corcoran <danielc.92@outlook.com>
2019-09-01 15:07:56 +1000

    Experimented with dropdown properties

--- src/components/jobs/ReactJobPostContainer.js
+++ src/components/jobs/ReactJobPostContainer.js
@@ -1,23 +1,58 @@
 import React, { Component } from 'react';
-import { Segment, Container, Header, Form, Button } from 'semantic-ui-react';
+import { Segment, Container, Header, Form, Message } from 'semantic-ui-react';
 
 
 export default class ReactJobPostContainer extends Component {
 
     state = {
-        skills: [{id:1, name: 'teamwork'},
-    {id:2, name:'friendship'},
-{id: 3, name: 'jumping'}]
+        skills: [
+            {key: 'lsiadyufgaose873', text: 'teamwork', value: 'lsiadyufgaose873'},
+            {key: '3w', text: 'jumping', value: '3w'},
+            {key: 'aaa', text: 'running', value: 'aaa'},
+            {key: 'aa', text: 'rowing', value: 'aa'},
+            {key: 'fd', text: 'communication', value: 'fd'},
+            {key: 'ef', text: 'negotiation', value: 'ef'},
+            {key: 'asdf', text: 'finance', value: 'asdf'}
+        ],
+        benefits: [
+            {key: '1', value: '1', text: 'sick leave'},
+            {key: '2', value: '2', text: 'paternal leave'},
+            {key: '12', value: '12', text: 'flexible hours'}
+        ]
     }
     render() {
         return (
             <Container>
                 <Segment style={{ padding: '7rem 0', border: 'none', boxShadow: 'none', margin: 'none'}}>
                     <Header as="h1">Post a job</Header>
-                    <p>Note you must be logged in to create a job post.</p>
+                    <Message
+                    color="yellow"
+                    header="Alert"
+                    content="In order to post a job you need to be authenticated."
+                    />
                     <Form>
-                        <Form.Input 
+                        <Form.Input
+                        placeholder="Zoo keeper"
                         label="Job Title"/>
+                        
+                        <Form.Dropdown 
+                            label="Skills"
+                            placeholder='Add skills'
+                            multiple
+                            search
+                            selection
+                            options={this.state.skills}
+                            ></Form.Dropdown>
+
+                        <Form.Dropdown 
+                            label="Benefits"
+                            placeholder='Add benefits'
+                            multiple
+                            search
+                            selection
+                            options={this.state.benefits}
+                            ></Form.Dropdown>
+
 
                         <Form.TextArea
                         placeholder="A short description about the company hiring"
@@ -27,20 +62,20 @@ export default class ReactJobPostContainer extends Component {
                         placeholder="A short description about the job"
                         label="Job summary"/>
 
-                        <Form.Input 
-                        type="number"
-                        label="Minimum salary ($)"/>
-
-                        <Form.Input 
-                        type="number"
-                        label="Maximum salary ($)"/>
-
                         
-                        <Form.Dropdown
-                        label="Select skills"
-                        ></Form.Dropdown>
+                        <Form.Group>
+                            <Form.Input 
+                            type="number"
+                            label="Minimum salary ($)"/>
+
+                            <Form.Input 
+                            type="number"
+                            label="Maximum salary ($)"/>
+                        </Form.Group>
+                        
                       
                         <Form.Button 
+                        icon="home"
                         size="big"
                         color="green">Create Job</Form.Button>
                     </Form>
