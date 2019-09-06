e0bb8ca396bfe0492e81c1b3d8ad13698ec5a433
Daniel Corcoran <danielc.92@outlook.com>
2019-09-06 20:48:06 +1000

    Protected job post form with authenticated flag

--- src/components/jobs/ReactJobPostContainer.js
+++ src/components/jobs/ReactJobPostContainer.js
@@ -1,13 +1,12 @@
 import React, { Component } from 'react';
-import { Segment, Container, Header, Form, Message, Icon } from 'semantic-ui-react';
+import { Segment, Container, Header, Form, Message, Icon, Divider } from 'semantic-ui-react';
 import { connect } from 'react-redux';
 import { setMenuItem } from '../../actions/menu';
 import { getSkills } from '../../actions/skills';
 import { getBenefits } from '../../actions/benefit';
 import { getCategories } from '../../actions/category';
-import axios from 'axios';
 import { createJob } from '../../actions/job'; 
-
+import { Redirect } from 'react-router'
 
 class ReactJobPostContainer extends Component {
 
@@ -70,114 +69,133 @@ class ReactJobPostContainer extends Component {
         this.props.propsGetSkills()
         this.props.propsGetCategories()
         this.props.propsGetBenefits()
+        
     }
     
     render() {
-        const  { benefit, skill, category, job } = this.props;
+
+        const  { 
+            auth,
+            benefit,
+            category,
+            job,
+            skill } = this.props;
+
         return (
             <Container>
                 <Segment style={{ padding: '7rem 0', border: 'none', boxShadow: 'none', margin: 'none'}}>
                     <Header as="h1">Post a job</Header>
-                    <Message
-                    color="yellow"
-                    header="Alert"
-                    content="In order to post a job you need to be authenticated."
-                    />
-                    <Form onSubmit={this.handleSubmit}>
-                        <Form.Input
-                            onChange={this.handleInputChange}
-                            name="title"
-                            placeholder="Zoo keeper"
-                            label="Job Title"/>
+                    <Divider></Divider>
+                    
+                    {
+                        auth.isAuthenticated ?
+                        (
+                            <Form onSubmit={this.handleSubmit}>
+                                <Form.Input
+                                    onChange={this.handleInputChange}
+                                    name="title"
+                                    placeholder="Zoo keeper"
+                                    label="Job Title"/>
 
-                        <Form.Dropdown
-                            onChange={this.handleDropdownChange}
-                            name="category"
-                            label="Category"
-                            placeholder="Select category"
-                            fluid
-                            selection
-                            search
-                            options={category.data}
-                        />
-                        
-                        <Form.Dropdown 
-                            onChange={this.handleDropdownChange}
-                            name="skills"
-                            label="Skills"
-                            placeholder='Add skills'
-                            multiple
-                            search
-                            selection
-                            options={skill.data}
-                            ></Form.Dropdown>
+                                <Form.Dropdown
+                                    onChange={this.handleDropdownChange}
+                                    name="category"
+                                    label="Category"
+                                    placeholder="Select category"
+                                    fluid
+                                    selection
+                                    search
+                                    options={category.data}
+                                />
+                                
+                                <Form.Dropdown 
+                                    onChange={this.handleDropdownChange}
+                                    name="skills"
+                                    label="Skills"
+                                    placeholder='Add skills'
+                                    multiple
+                                    search
+                                    selection
+                                    options={skill.data}
+                                    ></Form.Dropdown>
 
-                        <Form.Dropdown 
-                            onChange={this.handleDropdownChange}
-                            name="benefits"
-                            label="Benefits"
-                            placeholder='Add benefits'
-                            multiple
-                            search
-                            selection
-                            options={benefit.data}
-                            ></Form.Dropdown>
+                                <Form.Dropdown 
+                                    onChange={this.handleDropdownChange}
+                                    name="benefits"
+                                    label="Benefits"
+                                    placeholder='Add benefits'
+                                    multiple
+                                    search
+                                    selection
+                                    options={benefit.data}
+                                    ></Form.Dropdown>
 
-                        <Form.TextArea
-                            onChange={this.handleInputChange}
-                            name="company_summary"
-                            maxlength="500"
-                            placeholder="A short description about the company"
-                            label="About the company"/>
+                                <Form.TextArea
+                                    onChange={this.handleInputChange}
+                                    name="company_summary"
+                                    maxlength="500"
+                                    placeholder="A short description about the company"
+                                    label="About the company"/>
 
-                        <Form.TextArea 
-                            onChange={this.handleInputChange}
-                            name="job_summary"
-                            maxlength="500"
-                            placeholder="A short description about the job"
-                            label="About the job"/>
+                                <Form.TextArea 
+                                    onChange={this.handleInputChange}
+                                    name="job_summary"
+                                    maxlength="500"
+                                    placeholder="A short description about the job"
+                                    label="About the job"/>
 
-                        <Form.TextArea
-                            onChange={this.handleInputChange}
-                            name="contact_summary"
-                            maxlength="500"
-                            placeholder="Enter any contact details..."
-                            label="Contact details"/>           
+                                <Form.TextArea
+                                    onChange={this.handleInputChange}
+                                    name="contact_summary"
+                                    maxlength="500"
+                                    placeholder="Enter any contact details..."
+                                    label="Contact details"/>           
 
-                        <Form.Group>
-                            <Form.Input 
-                            onChange={this.handleInputChange}
-                            name="salary_range_low"
-                            type="number"
-                            label="Minimum salary ($)"/>
+                                <Form.Group>
+                                    <Form.Input 
+                                    onChange={this.handleInputChange}
+                                    name="salary_range_low"
+                                    type="number"
+                                    label="Minimum salary ($)"/>
 
-                            <Form.Input 
-                            onChange={this.handleInputChange}
-                            name="salary_range_high"
-                            type="number"
-                            label="Maximum salary ($)"/>
-                        </Form.Group>
-                        
-                        <Form.Button 
-                        size="big"
-                        color="green">
-                            <Icon name="add square"></Icon>Create job
-                        </Form.Button>
+                                    <Form.Input 
+                                    onChange={this.handleInputChange}
+                                    name="salary_range_high"
+                                    type="number"
+                                    label="Maximum salary ($)"/>
+                                </Form.Group>
+                                
+                                <Form.Button 
+                                size="big"
+                                color="green">
+                                    <Icon name="add square"></Icon>Create job
+                                </Form.Button>
 
-                        <Message
-                            error
-                            visible={job.error}
-                            header="Error"
-                            content="An error has occured, job posting failed">
-                        </Message>
+                                <Message
+                                    error
+                                    visible={job.error}
+                                    header="Error"
+                                    content="An error has occured, job posting failed">
+                                </Message>
 
-                        <Message
-                            success
-                            visible={Object.entries(job.data).length > 0}
-                            header="Success"
-                            content="Your job has been posted.">
-                        </Message>
-                    </Form>
+                                <Message
+                                    success
+                                    visible={Object.entries(job.data).length > 0}
+                                    header="Success"
+                                    content="Your job has been posted.">
+                                </Message>
+                            </Form>
+                        )
+                        :
+                        (
+                            <Message 
+                            warning
+                            header="Authentication required"
+                            content="We know you would love to get your job posted on our platform. However, you must be logged in to do so."
+                            >
+                            </Message>
+                        )
+                    }
                 </Segment>
             </Container>
         )
