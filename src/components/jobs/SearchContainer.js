4a728b2be4009ee8b93f1832f0586fe82b7539c7
daniel corcoran <danielc.92@outlook.com>
2019-12-23 11:21:05 +1100

    Added placeholders

--- src/components/jobs/SearchContainer.js
+++ src/components/jobs/SearchContainer.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { Segment, Container, Header, Input } from 'semantic-ui-react';
+import { Segment, Container, Header, Form, Icon } from 'semantic-ui-react';
 
 export default class SearchContainer extends Component {
 
@@ -7,13 +7,38 @@ export default class SearchContainer extends Component {
         search: ''
     }
 
+    handleInputChange = (e, {value}) => {
+        console.log(value);
+    }
 
     render() {
         return (
             <Segment color="green" inverted style={{borderRadius: '0', padding: '3rem 2rem', margin: '0'}}>
                 <Container>
-                    <Header as="h1" style={{color: '#fff'}}>Find your job.</Header>
-                    <Input onChange={(e)=> this.setState({search: e.target.value}) } size="big" action='Search' placeholder='Enter some keywords...' />
+                    <Header as="h1" style={{color: '#fff'}}>Find your dream job.</Header>
+                    
+                    <Form>
+                        <Form.Group>
+                            <Form.Input
+                                label="What" 
+                                onChange={this.handleInputChange} 
+                                placeholder='Enter some keywords...'
+                            />
+                            <Form.Dropdown
+                                label="Where"
+                                placeholder="Enter location, postcode, state"
+                                search
+                                selection
+                            />
+                            
+                        </Form.Group>
+
+                        <Form.Button
+                            size="huge"
+                            color="white">
+                            <Icon name="search"></Icon>Search
+                        </Form.Button>
+                    </Form>
                 </Container>
             </Segment>
         )
