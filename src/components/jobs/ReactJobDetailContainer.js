c7b753f95d2bc40261337f7c5c52adfae8d2ca29
daniel corcoran <danielc.92@outlook.com>
2020-01-01 12:29:31 +1100

    Added placeholder for job detail container

---
+++ src/components/jobs/ReactJobDetailContainer.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import { Segment, Header } from 'semantic-ui-react';
+
+export default class ReactJobDetailContainer extends Component {
+    render() {
+        return (
+            <Segment basic color="purple" inverted>
+                <Header>Job detail container</Header>
+            </Segment>
+        )
+    }
+}
