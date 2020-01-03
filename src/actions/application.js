ae641f7c94f61adba11b7f91d8a00645239140ec
daniel corcoran <danielc.92@outlook.com>
2020-01-03 21:48:02 +1100

    created action for application

---
+++ src/actions/application.js
@@ -0,0 +1,27 @@
+import jobApi from '../api';
+import { TOKEN_NAME } from '../constants';
+
+export const createApplication = (payload) => async (dispatch, getState) => {
+    try {
+        const token = localStorage.getItem(TOKEN_NAME)
+        const config = { headers: { 'x-access-token' : token } }
+        const response = await jobApi.post('application', payload, config)
+        dispatch({
+            type: "APPLICATION_SUCCESS",
+            payload: {
+                error: false,
+                flag: true,
+            }
+        })
+    } 
+    catch (error) {
+        dispatch({
+            type: "APPLICATION_FAILURE",
+            payload: {
+                error: true,
+                flag: false,
+                errorMessage: error.response.data.error
+            }
+        })
+    }
+}
