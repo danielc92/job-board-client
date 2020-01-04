12090ff9493535fc1958ed03a099698ae6a54a3c
daniel corcoran <danielc.92@outlook.com>
2020-01-04 14:15:17 +1100

    Time function

--- src/components/profile/listings/JobSeekerComponent.js
+++ src/components/profile/listings/JobSeekerComponent.js
@@ -13,6 +13,24 @@ class JobSeekerComponent extends Component {
         this.props.propsGetApplicationList(query)
     }
 
+    dateDiffString(oldDate) {
+        const minutesPerDay = 1440
+        const previous = new Date(oldDate)
+        const now = new Date();
+        const difference = now - previous;
+        const minutes = Math.floor(difference / 1000 / 60)
+        if (minutes < 60) {
+            return `${minutes} minutes ago`
+        }
+        else if (minutes < minutesPerDay){
+            const hours = Math.floor(minutes / 60)
+            return `${hours} hours ago`
+        } else {
+            const days = Math.floor(minutes/60/24)
+            return `${days} days ago`
+        }
+    }
+
     render() {
         const { application_list } = this.props;
         const { data } = application_list;
@@ -27,7 +45,7 @@ class JobSeekerComponent extends Component {
                             <Table.Header>
                                 <Table.Row>
                                     <Table.HeaderCell>Status</Table.HeaderCell>
-                                    <Table.HeaderCell>Applied on</Table.HeaderCell>
+                                    <Table.HeaderCell>Applied</Table.HeaderCell>
                                     <Table.HeaderCell>Rating</Table.HeaderCell>
                                     <Table.HeaderCell>Action</Table.HeaderCell>
                                 </Table.Row>
@@ -38,10 +56,10 @@ class JobSeekerComponent extends Component {
                                     data.map(item => {
                                         return (
                                         <Table.Row>
-                                            <Table.Cell><Label color="green">{ item.status }</Label></Table.Cell>
-                                            <Table.Cell>{ Date(item.createdAt) }</Table.Cell>
+                                            <Table.Cell><Label>{ item.status }</Label></Table.Cell>
+                                            <Table.Cell>{ this.dateDiffString(item.createdAt) }</Table.Cell>
                                             <Table.Cell>{ item.rating }</Table.Cell>
-                                            <Table.Cell><Button>Withdraw my application</Button></Table.Cell>
+                                            <Table.Cell><Button size="small" color="violet">Withdraw my application</Button></Table.Cell>
                                         </Table.Row>
                                         )
                                     })
