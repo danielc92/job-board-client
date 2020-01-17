8d1731fd3ef5d642d43160e47631e59d87c1dfa2
Daniel Corcoran <danielc.92@outlook.com>
2020-01-18 08:59:54 +1100

    Changed benefit endpoint

--- src/actions/benefit.js
+++ src/actions/benefit.js
@@ -8,7 +8,7 @@ export const getBenefits = () => async (dispatch, getState) => {
     const token = localStorage.getItem(TOKEN_NAME)
 
     try {
-        const response = await jobApi.get('benefit', 
+        const response = await jobApi.get('benefit/list', 
         { headers : {'x-access-token' : token }})
 
         // Data needs to be transformed to meet structure from Semantic's <Dropdown>
