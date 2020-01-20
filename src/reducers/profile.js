046a6043222014be7c0b0bfdf2467997f79930f8
Daniel Corcoran <danielc.92@outlook.com>
2020-01-20 21:59:27 +1100

    Refactor profile container

--- src/reducers/profile.js
+++ src/reducers/profile.js
@@ -1,9 +1,11 @@
 export const profileReducer = (state={ error: false, loaded: false}, action) => {
     const { type, payload } = action;
     switch(type) {
-        case 'FETCH_USER_DETAIL_SUCCESS':
+        case 'GET_USER_DETAIL_FETCHING':
             return payload
-        case 'FETCH_USER_DETAIL_FAILURE':
+        case 'GET_USER_DETAIL_SUCCESS':
+            return payload
+        case 'GET_USER_DETAIL_FAILURE':
             return payload
         default:
             return state
