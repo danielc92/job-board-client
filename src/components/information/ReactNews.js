743957f79b735ffb28f38d76ea87f018493b8849
Daniel Corcoran <danielc.92@outlook.com>
2020-01-25 19:05:07 +1100

    Added news 'read more' button

--- src/components/information/ReactNews.js
+++ src/components/information/ReactNews.js
@@ -1,43 +1,57 @@
-import React, { Component } from 'react';
-import { Segment, Container, Label,  Divider, Header} from 'semantic-ui-react';
-import { connect } from 'react-redux';
-import { setMenuItem } from '../../actions/menu';
-import VerticallyPaddedContainer from '../layout/VerticallyPaddedContainer';
+import React, { Component } from 'react'
+import {
+  Segment,
+  Container,
+  Label,
+  Icon,
+  Divider,
+  Header,
+  Button,
+} from 'semantic-ui-react'
+import { connect } from 'react-redux'
+import { setMenuItem } from '../../actions/menu'
+import VerticallyPaddedContainer from '../layout/VerticallyPaddedContainer'
 
 class ReactNews extends Component {
+  componentDidMount() {
+    this.props.propsSetMenuItem('news')
+  }
 
-    componentDidMount() {
-        this.props.propsSetMenuItem('news')
-    }
-
-    render() {
-        return (
-            <div>
-                <Segment basic>
-                    <Container>
-                        <VerticallyPaddedContainer size="4">
-                            <Header as="h1">News</Header>
-                            <p>Read about the latest updates and progress.</p>
-                            <Divider/>
-                            {
-                                new Array(3).fill(null).map(x => (
-                                    <Segment stacked color="green">
-                                    <Header as="h3">Culpa non eiusmod et dolor.</Header>
-                                    <p>Mollit ng non fugiat dolore sunodo duis. Aliquip ipsum pariatur ametsse deserunt.</p>
-                                    <Label size="small">Created on 20th August 2019</Label>
-                                    </Segment>
-                                ))
-                            }      
-                        </VerticallyPaddedContainer>
-                    </Container>
+  render() {
+    return (
+      <div>
+        <Segment basic>
+          <Container>
+            <VerticallyPaddedContainer size="4">
+              <Header as="h1">News</Header>
+              <p>Read about the latest updates and progress.</p>
+              <Divider />
+              {new Array(3).fill(null).map(x => (
+                <Segment stacked color="green">
+                  <Header as="h3">Culpa non eiusmod et dolor.</Header>
+                  <p>
+                    Mollit ng non fugiat dolore sunodo duis. Aliquip ipsum
+                    pariatur ametsse deserunt.
+                  </p>
+                  <Label.Group>
+                    <Label size="tiny" content="Created on 20th August 2019" />
+                  </Label.Group>
+                  <Button color="green">
+                    <Icon name="book" />
+                    Read more
+                  </Button>
                 </Segment>
-            </div>
-        )
-    }
+              ))}
+            </VerticallyPaddedContainer>
+          </Container>
+        </Segment>
+      </div>
+    )
+  }
 }
 
-const mapDispatchToProps = { 
-    propsSetMenuItem: setMenuItem 
+const mapDispatchToProps = {
+  propsSetMenuItem: setMenuItem,
 }
 
-export default connect(null, mapDispatchToProps)(ReactNews);
+export default connect(null, mapDispatchToProps)(ReactNews)
