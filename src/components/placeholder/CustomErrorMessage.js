fa32ab5c7fc4c65ebe623d5acfa481d48eedd291
Daniel Corcoran <danielc.92@outlook.com>
2020-01-26 13:32:16 +1100

    Added refresh option to error state

--- src/components/placeholder/CustomErrorMessage.js
+++ src/components/placeholder/CustomErrorMessage.js
@@ -1,12 +1,23 @@
 import React, { Fragment } from 'react'
-import { Image, Message } from 'semantic-ui-react'
+import { Icon, Image, Message, Button } from 'semantic-ui-react'
 import src from '../../images/page_not_found_su7k.svg'
 
 export default function CustomErrorMessage(props) {
   return (
-    <Fragment>
+    <section
+      style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}
+    >
       <Image style={{ maxWidth: '375px' }} centered fluid src={src} />
       <Message color="red" header={props.header} content={props.content} />
-    </Fragment>
+      <Button
+        centered
+        color="green"
+        size="big"
+        onClick={() => window.location.reload()}
+      >
+        <Icon name="refresh" />
+        Refresh Page
+      </Button>
+    </section>
   )
 }
