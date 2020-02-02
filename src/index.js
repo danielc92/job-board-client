15ca8f47aae42241c5c3606a056242ea22740d03
Daniel Corcoran <danielc.92@outlook.com>
2020-02-02 21:54:35 +1100

    Added nunito font

--- src/index.js
+++ src/index.js
@@ -1,24 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './components/App';
-import { createStore, compose, applyMiddleware } from 'redux';
-import thunk from 'redux-thunk';
-import { Provider } from 'react-redux';
-import { rootReducer } from './reducers';
-import { BrowserRouter } from 'react-router-dom';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './components/App'
+import { createStore, compose, applyMiddleware } from 'redux'
+import thunk from 'redux-thunk'
+import { Provider } from 'react-redux'
+import { rootReducer } from './reducers'
+import { BrowserRouter } from 'react-router-dom'
+import './index.css'
 
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
 
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-
-const store = createStore(rootReducer, /* preloaded state, */ composeEnhancers(
-    applyMiddleware(thunk)
-))
+const store = createStore(
+  rootReducer,
+  /* preloaded state, */ composeEnhancers(applyMiddleware(thunk))
+)
 
 ReactDOM.render(
-    <Provider store={store}>
-        <BrowserRouter forceRefresh={false}>
-            <App/>
-        </BrowserRouter>
-    </Provider>
-    , 
-    document.getElementById('root'));
+  <Provider store={store}>
+    <BrowserRouter forceRefresh={false}>
+      <App />
+    </BrowserRouter>
+  </Provider>,
+  document.getElementById('root')
+)
