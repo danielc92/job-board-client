485c734152218c69943382bc52e0c59080c7fa16
Daniel Corcoran <danielc.92@outlook.com>
2020-02-10 21:25:52 +1100

    Fixed bug in error handling

--- src/reducers/register.js
+++ src/reducers/register.js
@@ -1,29 +1,31 @@
-export const registerReducer = (state={ error: false, flag: false, message: ''}, action) => {
-    
-    const { type, payload } = action
+export const registerReducer = (
+  state = { error: false, flag: false, message: '' },
+  action
+) => {
+  const { type, payload } = action
 
-    switch(type) {
-        case 'REGISTER_SUCCESS':
-            return {
-                ...state,
-                flag: true,
-                error: false,
-                message: payload.data.message
-            }
-        case 'REGISTER_FAILURE':
-            return {
-                ...state,
-                flag: false,
-                error: true,
-                message: payload.error
-            }
-        case 'REGISTER_RESET':
-            return {
-                ...state,
-                flag: false,
-                error: false,
-            }
-        default:
-            return state
-    }
+  switch (type) {
+    case 'REGISTER_SUCCESS':
+      return {
+        ...state,
+        flag: true,
+        error: false,
+        message: payload.data.message,
+      }
+    case 'REGISTER_FAILURE':
+      return {
+        ...state,
+        flag: false,
+        error: true,
+        message: payload.message,
+      }
+    case 'REGISTER_RESET':
+      return {
+        ...state,
+        flag: false,
+        error: false,
+      }
+    default:
+      return state
+  }
 }
