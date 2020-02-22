223d3a19be984d8a96e9a6c48cd34810f16f5516
Daniel Corcoran <danielc.92@outlook.com>
2020-02-23 10:07:29 +1100

    Added feedback action

---
+++ src/actions/feedback.js
@@ -0,0 +1,42 @@
+import jobApi from '../../api'
+import { handleApiError } from '../../helpers/api'
+import { getConfig } from '../helpers/api'
+
+export const createFeedback = payload => async (dispatch, getState) => {
+  try {
+    dispatch({
+      type: 'CREATE_FEEDBACK_LOADING',
+      payload: {
+        loading: true,
+        header: 'Processing',
+        message: 'Please wait while we process your request.',
+      },
+    })
+    const config = getConfig()
+    const response = await jobApi.post('feedback', config, payload)
+    dispatch({
+      type: 'CREATE_FEEDBACK_LOADING',
+      payload: {
+        error: false,
+        message: response.data.message,
+        header: 'Success',
+      },
+    })
+  } catch (error) {
+    dispatch({
+      type: 'CREATE_FEEDBACK_LOADING',
+      payload: {
+        error: true,
+        message: handleApiError(error),
+        header: 'An error has occured',
+      },
+    })
+  }
+}
+
+export const resetCreateFeedback = payload => async (dispatch, getState) => {
+  dispatch({
+    type: 'CREATE_FEEDBACK_RESET',
+    payload: { error: false, modalIsClosed: true },
+  })
+}
