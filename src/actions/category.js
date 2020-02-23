07f0de467fe635abfb3d1706c3c170602c99de84
Daniel Corcoran <danielc.92@outlook.com>
2020-02-23 14:32:52 +1100

    Proper case transforms on category and locations

--- src/actions/category.js
+++ src/actions/category.js
@@ -1,35 +1,32 @@
-import jobApi from '../api';
-import { handleApiError } from '../helpers/api';
+import jobApi from '../api'
+import { handleApiError } from '../helpers/api'
+import { properCaseTransform } from '../helpers/generic'
 
 export const getCategories = () => async (dispatch, getState) => {
-    
-    try {
-        const response = await jobApi.get('category/list')
+  try {
+    const response = await jobApi.get('category/list')
 
-        // Data needs to be transformed to meet structure from Semantic's <Dropdown>
-        const data = response.data.map(record => ({
-            text: record.name,
-            value: record.name,
-            key: record._id
-        }))
+    // Data needs to be transformed to meet structure from Semantic's <Dropdown>
+    const data = response.data.map(record => ({
+      text: properCaseTransform(record.name),
+      value: record.name,
+      key: record._id,
+    }))
 
-        dispatch({
-            type: "GET_CATEGORIES_SUCCESS",
-            payload: {
-                data,
-                error: false,
-            }
-        })
-    }
-    catch(error) {
-
-        dispatch({
-            type: "GET_CATEGORIES_FAILURE",
-            payload: {
-                message: handleApiError(error),
-                error: true,
-            }
-        })
-    }
-
+    dispatch({
+      type: 'GET_CATEGORIES_SUCCESS',
+      payload: {
+        data,
+        error: false,
+      },
+    })
+  } catch (error) {
+    dispatch({
+      type: 'GET_CATEGORIES_FAILURE',
+      payload: {
+        message: handleApiError(error),
+        error: true,
+      },
+    })
+  }
 }
--- src/actions/location.js
+++ src/actions/location.js
@@ -1,35 +1,36 @@
-import jobApi from '../api';
-import { handleApiError } from '../helpers/api';
+import jobApi from '../api'
+import { handleApiError } from '../helpers/api'
+import { properCaseTransform } from '../helpers/generic'
 
-export const getLocationList = (search) => async (dispatch, getState) => {
-    try {   
-        const url = `location/list?search=${search}`
-        const response = await jobApi.get(url)
-        
-        // Transform locations for semantic component
-        const data = response.data.map((item, index) => {
-            return {
-                key: index.toString(),
-                text: item.location_string,
-                value: item
-            }
-        })
-        dispatch({
-            type: "GET_LOCATION_LIST_SUCCESS",
-            payload: {
-                data,
-                error: false,
-                search,
-            }
-        })
-    } catch (error) {
-        dispatch({
-            type: "GET_LOCATION_LIST_FAILURE",
-            payload: {
-                error: true,
-                message: handleApiError(error),
-                search,
-            }
-        })
-    }
+export const getLocationList = search => async (dispatch, getState) => {
+  try {
+    const url = `location/list?search=${search}`
+    const response = await jobApi.get(url)
+
+    // Transform locations for semantic component
+    const data = response.data.map((item, index) => {
+      return {
+        key: index.toString(),
+        text: properCaseTransform(item.location_string),
+        value: item,
+      }
+    })
+    dispatch({
+      type: 'GET_LOCATION_LIST_SUCCESS',
+      payload: {
+        data,
+        error: false,
+        search,
+      },
+    })
+  } catch (error) {
+    dispatch({
+      type: 'GET_LOCATION_LIST_FAILURE',
+      payload: {
+        error: true,
+        message: handleApiError(error),
+        search,
+      },
+    })
+  }
 }
