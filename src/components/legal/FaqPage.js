de02e959025366308202d52256e9f28ba9694d40
Daniel Corcoran <danielc.92@outlook.com>
2020-03-05 20:43:32 +1100

    Hooked up documentation api

--- src/components/legal/FaqPage.js
+++ src/components/legal/FaqPage.js
@@ -1,34 +1,69 @@
-import React, { Component } from 'react'
-import { Header, Container } from 'semantic-ui-react'
+import React, { Component, Fragment } from 'react'
+import { Container, Segment, Placeholder } from 'semantic-ui-react'
 import VerticallyPaddedContainer from 'components/layout/VerticallyPaddedContainer'
+import { connect } from 'react-redux'
+import { getDocumentation } from 'actions/documentation'
+import CustomErrorMessage from 'components/reusable/CustomErrorMessage'
+import { documentationRenderer } from 'helpers/renderer'
 
-export default class FaqPage extends Component {
+class FaqPage extends Component {
+  componentDidMount() {
+    const { propsGetDocumentation, documentation } = this.props
+    if (!documentation.data) {
+      propsGetDocumentation()
+    }
+  }
+
   render() {
+    const { documentation } = this.props
+
     return (
       <Container>
-        <VerticallyPaddedContainer size="3">
-          <Header as="h1" content="Frequently asked questions (FAQ)" />
-          <p>
-            Proident ipsum amet eu ut magna ex reprehenderit excepteur nisi
-            dolore anim veniam commodo id. Sit excepteur ex ad elit consectetur
-            elit elit aliqua excepteur nulla excepteur in. Fugiat in consectetur
-            amet aliqua ullamco ea elit ex tempor non. Aliqua Lorem et aliquip
-            quis quis. Quis ipsum laboris eiusmod laboris qui quis labore culpa
-            cupidatat nisi Lorem non. Pariatur eu eiusmod magna mollit commodo
-            consequat irure laboris. Ad pariatur ipsum enim ullamco laborum.
-          </p>
-          <Header as="h3" content="FAQ" />
-          <p>
-            Proident ipsum amet eu ut magna ex reprehenderit excepteur nisi
-            dolore anim veniam commodo id. Sit excepteur ex ad elit consectetur
-            elit elit aliqua excepteur nulla excepteur in. Fugiat in consectetur
-            amet aliqua ullamco ea elit ex tempor non. Aliqua Lorem et aliquip
-            quis quis. Quis ipsum laboris eiusmod laboris qui quis labore culpa
-            cupidatat nisi Lorem non. Pariatur eu eiusmod magna mollit commodo
-            consequat irure laboris. Ad pariatur ipsum enim ullamco laborum.
-          </p>
+        <VerticallyPaddedContainer size="4">
+          {documentation.error ? (
+            <CustomErrorMessage content="An error has occured" />
+          ) : documentation.loading ? (
+            <Fragment>
+              <Placeholder fluid>
+                <Placeholder.Header>
+                  <Placeholder.Line />
+                  <Placeholder.Line />
+                </Placeholder.Header>
+              </Placeholder>
+              <Segment stacked padded>
+                <Placeholder fluid>
+                  <Placeholder.Paragraph>
+                    <Placeholder.Line /> <Placeholder.Line />{' '}
+                    <Placeholder.Line /> <Placeholder.Line />{' '}
+                    <Placeholder.Line /> <Placeholder.Line />{' '}
+                    <Placeholder.Line />
+                    <Placeholder.Line /> <Placeholder.Line />{' '}
+                    <Placeholder.Line /> <Placeholder.Line />
+                    <Placeholder.Line /> <Placeholder.Line />{' '}
+                    <Placeholder.Line /> <Placeholder.Line />{' '}
+                    <Placeholder.Line /> <Placeholder.Line />
+                  </Placeholder.Paragraph>
+                </Placeholder>
+              </Segment>
+            </Fragment>
+          ) : (
+            documentationRenderer(documentation.data, 'faq')
+          )}
         </VerticallyPaddedContainer>
       </Container>
     )
   }
 }
+
+const mapStateToProps = state => {
+  const { documentation } = state
+  return {
+    documentation,
+  }
+}
+
+const mapDispatchToProps = {
+  propsGetDocumentation: getDocumentation,
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(FaqPage)
