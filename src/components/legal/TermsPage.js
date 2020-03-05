a8c0eb8d4c53545108ea03b153177967aea664b2
Daniel Corcoran <danielc.92@outlook.com>
2020-03-05 20:43:41 +1100

    Hooked up documentation api

--- src/components/legal/TermsPage.js
+++ src/components/legal/TermsPage.js
@@ -1,17 +1,68 @@
-import React, { Component } from 'react'
-import { Header, Container } from 'semantic-ui-react'
+import React, { Component, Fragment } from 'react'
+import { Container, Segment, Placeholder } from 'semantic-ui-react'
 import VerticallyPaddedContainer from 'components/layout/VerticallyPaddedContainer'
-export default class TermsPage extends Component {
+import { connect } from 'react-redux'
+import { getDocumentation } from 'actions/documentation'
+import CustomErrorMessage from 'components/reusable/CustomErrorMessage'
+import { documentationRenderer } from 'helpers/renderer'
+class TermsPage extends Component {
+  componentDidMount() {
+    const { propsGetDocumentation, documentation } = this.props
+    if (!documentation.data) {
+      propsGetDocumentation()
+    }
+  }
+
   render() {
+    const { documentation } = this.props
+
     return (
       <Container>
-        <VerticallyPaddedContainer size="3">
-          <Header as="h1" content="Terms of use" />
-          <p>There is nothing here yet...</p>
-          <Header as="h3" content="Sub heading" />
-          <p>There is nothing here yet.</p>
+        <VerticallyPaddedContainer size="4">
+          {documentation.error ? (
+            <CustomErrorMessage content="An error has occured" />
+          ) : documentation.loading ? (
+            <Fragment>
+              <Placeholder fluid>
+                <Placeholder.Header>
+                  <Placeholder.Line />
+                  <Placeholder.Line />
+                </Placeholder.Header>
+              </Placeholder>
+              <Segment stacked padded>
+                <Placeholder fluid>
+                  <Placeholder.Paragraph>
+                    <Placeholder.Line /> <Placeholder.Line />{' '}
+                    <Placeholder.Line /> <Placeholder.Line />{' '}
+                    <Placeholder.Line /> <Placeholder.Line />{' '}
+                    <Placeholder.Line />
+                    <Placeholder.Line /> <Placeholder.Line />{' '}
+                    <Placeholder.Line /> <Placeholder.Line />
+                    <Placeholder.Line /> <Placeholder.Line />{' '}
+                    <Placeholder.Line /> <Placeholder.Line />{' '}
+                    <Placeholder.Line /> <Placeholder.Line />
+                  </Placeholder.Paragraph>
+                </Placeholder>
+              </Segment>
+            </Fragment>
+          ) : (
+            documentationRenderer(documentation.data, 'terms-of-use')
+          )}
         </VerticallyPaddedContainer>
       </Container>
     )
   }
 }
+
+const mapStateToProps = state => {
+  const { documentation } = state
+  return {
+    documentation,
+  }
+}
+
+const mapDispatchToProps = {
+  propsGetDocumentation: getDocumentation,
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(TermsPage)
