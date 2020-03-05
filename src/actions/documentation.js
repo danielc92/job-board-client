ba2ba3352aaedbd56344c021f941f7a6d31ad9f2
Daniel Corcoran <danielc.92@outlook.com>
2020-03-05 20:43:53 +1100

    documentationi action

---
+++ src/actions/documentation.js
@@ -0,0 +1,33 @@
+import jobApi from 'api'
+import { handleApiError, getConfig } from 'helpers/api'
+
+export const getDocumentation = () => async (dispatch, getState) => {
+  const config = getConfig()
+  dispatch({
+    type: 'GET_DOCUMENTATION_LOADING',
+    payload: {
+      loading: true,
+      error: false,
+    },
+  })
+  await new Promise(r => setTimeout(r, 3500))
+  try {
+    const response = await jobApi.get('documentation/list', config)
+
+    dispatch({
+      type: 'GET_DOCUMENTATION_SUCCESS',
+      payload: {
+        data: response.data.results,
+        error: false,
+      },
+    })
+  } catch (error) {
+    dispatch({
+      type: 'GET_DOCUMENTATION_FAILURE',
+      payload: {
+        error: true,
+        message: handleApiError(error),
+      },
+    })
+  }
+}
