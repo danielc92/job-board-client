a0c58f9c01813a1324c27b7eaf41a6374dce15bc
Daniel Corcoran <danielc.92@outlook.com>
2020-03-07 13:01:20 +1100

    Updated renderer

--- src/helpers/renderer/index.js
+++ src/helpers/renderer/index.js
@@ -9,8 +9,15 @@ export const documentationRenderer = (data, key) => {
     if (c.node === 'heading-3') {
       return <Header as="h3" content={c.value} />
     }
+    if (c.node === 'heading-1') {
+      return <Header as="h1" content={c.value} />
+    }
     if (c.node === 'paragraph') {
       return <p>{c.value}</p>
+    }
+    if (c.node === 'unordered-list') {
+      const listItems = c.value.map(li => <li>{li}</li>)
+      return <ul>{listItems}</ul>
     }
     return <p>Unknown node.</p>
   })
