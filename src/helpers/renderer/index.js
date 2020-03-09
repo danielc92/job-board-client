982e3b9305f5ea659d88da9e246c05a3b0dd4ebd
Daniel Corcoran <danielc.92@outlook.com>
2020-03-09 15:22:33 +1100

    Added contentRenderer function

--- src/helpers/renderer/index.js
+++ src/helpers/renderer/index.js
@@ -1,11 +1,10 @@
 import { Header, Divider } from 'semantic-ui-react'
 import React, { Fragment } from 'react'
 
-export const documentationRenderer = (data, key) => {
+export const contentRenderer = data => {
   if (!data) return null
 
-  const content = data.find(c => c.route === key)
-  const items = content.content.map(c => {
+  const items = data.map(c => {
     if (c.node === 'heading-3') {
       return <Header as="h3" content={c.value} />
     }
@@ -19,8 +18,22 @@ export const documentationRenderer = (data, key) => {
       const listItems = c.value.map(li => <li>{li}</li>)
       return <ul>{listItems}</ul>
     }
+    if (c.node === 'ordered-list') {
+      const listItems = c.value.map(li => <li>{li}</li>)
+      return <ol>{listItems}</ol>
+    }
     return <p>Unknown node.</p>
   })
+  return items
+}
+
+export const documentationRenderer = (data, key) => {
+  if (!data) return null
+
+  const content = data.find(c => c.route === key)
+
+  const items = contentRenderer(content.content)
+
   return (
     <Fragment>
       <Header as="h1" content={content.title} />
