0cf95f3fbeec2b4d5189412715390fd072c46b5f
Daniel Corcoran <danielc.92@outlook.com>
2020-03-22 17:48:20 +1100

    Removed api constant

--- src/app_constants/index.js
+++ src/app_constants/index.js
@@ -1,6 +1,5 @@
 export const SITE_NAME = 'Career Hub'
 export const TOKEN_NAME = 'JOB_TOKEN'
-export const API_BASE_URL = 'http://localhost:3001/api/'
 export const SERVER_500_ERROR_MESAGE =
   'We apologise for the inconvenience. The server was unable to process your request.'
 export const SESSION_EXPIRED_MESSAGE =
