a180e096095edfa0478a52e8878529e00c98b8cf
Daniel Corcoran <danielc.92@outlook.com>
2020-05-22 20:09:36 +1000

    Modified renderer to account for images and additional margins

--- src/helpers/renderer/index.js
+++ src/helpers/renderer/index.js
@@ -1,27 +1,36 @@
 import { Header, Divider } from 'semantic-ui-react'
 import React, { Fragment } from 'react'
-import { Segment } from 'semantic-ui-react'
+import { Segment, Image } from 'semantic-ui-react'
 
-export const contentRenderer = data => {
+export const contentRenderer = (data) => {
   if (!data) return null
 
-  const items = data.map(c => {
+  const items = data.map((c) => {
     if (c.node === 'heading-3') {
-      return <Header as="h3" content={c.value} />
+      return (
+        <Header style={{ marginBottom: '24px' }} as="h3" content={c.value} />
+      )
     }
     if (c.node === 'heading-1') {
-      return <Header as="h1" content={c.value} />
+      return (
+        <Header style={{ marginBottom: '24px' }} as="h1" content={c.value} />
+      )
     }
     if (c.node === 'paragraph') {
-      return <p>{c.value}</p>
+      return <p style={{ marginBottom: '18px' }}>{c.value}</p>
     }
     if (c.node === 'unordered-list') {
-      const listItems = c.value.map(li => <li>{li}</li>)
-      return <ul>{listItems}</ul>
+      const listItems = c.value.map((li) => <li>{li}</li>)
+      return <ul style={{ marginBottom: '24px' }}>{listItems}</ul>
     }
     if (c.node === 'ordered-list') {
-      const listItems = c.value.map(li => <li>{li}</li>)
-      return <ol>{listItems}</ol>
+      const listItems = c.value.map((li) => <li>{li}</li>)
+      return <ol style={{ marginBottom: '24px' }}>{listItems}</ol>
+    }
+    if (c.node === 'image') {
+      return (
+        <Image style={{ marginBottom: '24px' }} src={c.value} size="large" />
+      )
     }
     return <p>Unknown node.</p>
   })
@@ -31,7 +40,7 @@ export const contentRenderer = data => {
 export const documentationRenderer = (data, key) => {
   if (!data) return null
 
-  const content = data.find(c => c.route === key)
+  const content = data.find((c) => c.route === key)
 
   const items = contentRenderer(content.content)
 
